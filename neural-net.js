8503aca2c474795c220e93f98f2cc1f672c26194
Eagan Martin <it-dev@scinnova.com.ph>
2019-08-30 19:31:05 +0800

    Removed unnecessary comments

--- neural-net.js
+++ neural-net.js
@@ -29,11 +29,6 @@ class NeuralNetwork {
     }
      
     forwardPropagation(input) {
-        console.log("##FORWARD");
-        console.log("Input: " + input);
-        console.log("synapseIH: " + this.synapseIH);
-        console.log("synapseHO: " + this.synapseHO);
-
         this.inputLayer = input;
         this.hiddenLayer = multiply(this.inputLayer, this.synapseIH).map(v => this.sigmoidActivation(v));
         this.outputLayer = multiply(this.hiddenLayer, this.synapseHO).map(v => this.sigmoidActivation(v));
@@ -68,63 +63,5 @@ class NeuralNetwork {
         return this.forwardPropagation(input);
     }
 }
-
-// let NeuralNetwork = function (config) {
-//     this.constructor(config);
-// };
-
-// NeuralNetwork.prototype.constructor = function (config) {
-//     this.inputNodes = config.inputNodes;
-//     this.hiddenNodes = config.HiddenLays;
-//     this.outputNodes = config.outputNodes;
-//     this.binaryThresh = config.binaryThresh;
-
-//     this.weightIH = nj.random([this.inputNodes, this.hiddenNodes]);
-//     this.wegithHO = nj.random([this.hiddenNodes, this.outputNodes]);
-
-//     return this;
-// };
-
-// NeuralNetwork.prototype.sigmoidActivation = function (value) {
-//     return 1 / (1 + nj.exp(-value));
-// };
-
-// NeuralNetwork.prototype.sigmoidPrime = function (value) {
-//     return value * (1 - value);
-// };
- 
-// NeuralNetwork.prototype.forwardPropagation = function (input) {
-//     console.log(this.self);
-//     console.log(input);
-//     console.log(this.weightIH);
-//     console.log(this.wegithHO);
-//     this.p1 = nj.dot(input, this.weightIH);
-//     this.s1 = this.sigmoidActivation(this.p1);
-//     this.p2 = nj.dot(this.s1, this.wegithHO);
-    
-//     let output = this.sigmoidActivation(p2);
-    
-//     return output;
-// };
-
-// NeuralNetwork.prototype.backwardPropagation = function (input, expected, output) {
-//     this.outputError = expected - output;
-//     this.outputDelta = this.outputError * this.sigmoidPrime(output);
-
-//     this.hiddenError = nj.dot(this.outputDelta, this.wegithHO.T);
-//     this.s1Delta = this.hiddenError * this.sigmoidPrime(this.s1);
-
-//     this.weightIH = matrixAddition(this.weightIH, nj.dot(input.T, this.s1Delta));
-//     this.wegithHO = matrixAddition(this.wegithHO, nj.dot(s1.T, this.outputDelta));
-// }
-
-// NeuralNetwork.prototype.train = function (input, expected) {
-//     let output = this.forwardPropagation(input);
-//     this.backwardPropagation(input, expected, output);
-// };
-
-// NeuralNetwork.prototype.predict = function (input) {
-//     this.forwardPropagation(input);
-// }
 
 module.exports = NeuralNetwork;
